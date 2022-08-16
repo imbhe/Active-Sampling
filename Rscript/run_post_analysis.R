f0cdc9aa7aeef6c074b234741437c0d652d9ddde
Kim <63707063+Kim-Y19@users.noreply.github.com>
2022-08-16 17:29:00 +0200

    update

--- Rscript/run_post_analysis.R
+++ Rscript/run_post_analysis.R
@@ -2,15 +2,12 @@
 # num_cases_per_iteration is limited by the real case number
 library("readxl")
 source("Rscript/active_learning.R")
-#library("tidyverse")
 source("Rscript/sim_output.R")
-load("Data/glance_dec_data_test.R")
-
+load("Data/glance_dec_data4.R")
 sampling_input <- read_excel("Data/Sampling_input2.xlsx")
-# have another table input for simulation number choice
 param_input <- read_excel("Data/param_input.xlsx")
-add_effn = 10
-nburnin = 1
+add_effn = 500
+nburnin = 0
 verbose = FALSE
 
 total_iter = ceiling(add_effn/param_input$num_cases_per_iteration)
@@ -20,7 +17,6 @@ res_total= replicate(length(param_input$Sim_n), data.frame())
 for(j in 1:length(param_input$Sim_n)){
   inputparameter <- data.frame(param_input$num_cases_per_iteration[j],total_iter[j], nburnin,param_input$total_nboot[j],verbose,param_input$Sim_n[j])
   colnames(inputparameter) <- c("num_cases_per_iteration","total_iter", "nburnin","total_nboot","verbose","Sim_n")
-  #set.seed(1)
   res_top_loop = replicate(tail(sampling_input$sim_order, 1), data.frame())
   
   for(i in 1:tail(sampling_input$sim_order, 1)){
@@ -32,8 +28,9 @@ for(j in 1:length(param_input$Sim_n)){
   print(paste("loop",j,"is finished"))
   res_total[[j]] <- res_top_loop
 }  
-  
-save(res_total, file = "Data/res_analysis_data_test2.R")
+
+save(res_total, file = "Data/New_sim_results17.R")
+
 
 
  
--- Rscript/sim_output.R
+++ Rscript/sim_output.R
@@ -10,7 +10,7 @@ sim_output <- function(df,input,inputparameter){
   res_list <- replicate(Sim_n, data.frame())
   for (k in 1:Sim_n){
     print(paste("simulation",k,"start,","reduced logic is",input$reduce_simulations_by_logic,", number per iteration:",num_cases_per_iteration,",total iteration:",total_iter))
-    #set.seed(k)
+    set.seed(k)
     out <- active_learning (df, sampling_method, 
                             proposal_dist,
                             target, 
@@ -25,47 +25,50 @@ sim_output <- function(df,input,inputparameter){
     res_list[[k]] <- out$results
   }
   res = do.call(rbind, res_list)
-  if(Sim_n>1){
-    aver = as.data.frame(matrix(nrow=total_iter,ncol=0))
-    biggest = as.data.frame(matrix(nrow=total_iter,ncol=0))
-    smallest = as.data.frame(matrix(nrow=total_iter,ncol=0))
-    index = matrix(nrow = total_iter,ncol = Sim_n)
-    for(p in 1:total_iter){
-      aver$group[p] = group
-      aver$reduce_simulations_by_logic[p] = reduce_simulations_by_logic
-      index[p,] = seq(p,total_iter*Sim_n,total_iter)
-      aver$neff0[p] = mean(res$neff0[index[p,]])
-      aver$iter[p] = mean(res$iter[p])
-      aver$mean_impact_speed0_sqerr[p] = sqrt(mean(res$mean_impact_speed0_sqerr[index[p,]]))
-      aver$absolute_impact_speed_reduction_sqerr[p] = sqrt(mean(res$absolute_impact_speed_reduction_sqerr[index[p,]]))
-      aver$proportion_crashes_avoided_sqerr[p] = sqrt(mean(res$proportion_crashes_avoided_sqerr[index[p,]]))
-      aver$absolute_injury_risk_reduction_sqerr[p] = sqrt(mean(res$absolute_injury_risk_reduction_sqerr[index[p,]]))
-      
-      biggest$group[p] = group
-      biggest$reduce_simulations_by_logic[p] = reduce_simulations_by_logic
-      biggest$iter[p] = mean(res$iter[p])
-      biggest$neff0[p] = max(res$neff0[index[p,]])
-      biggest$mean_impact_speed0_sqerr[p] = sqrt(max(res$mean_impact_speed0_sqerr[index[p,]]))
-      biggest$absolute_impact_speed_reduction_sqerr[p] = sqrt(max(res$absolute_impact_speed_reduction_sqerr[index[p,]]))
-      biggest$proportion_crashes_avoided_sqerr[p] = sqrt(max(res$proportion_crashes_avoided_sqerr[index[p,]]))
-      biggest$absolute_injury_risk_reduction_sqerr[p] = sqrt(max(res$absolute_injury_risk_reduction_sqerr[index[p,]]))
-      
-      
-      smallest$group[p] = group
-      smallest$reduce_simulations_by_logic[p] = reduce_simulations_by_logic
-      smallest$iter[p] = mean(res$iter[p])
-      smallest$neff0[p] = min(res$neff0[index[p,]])
-      smallest$mean_impact_speed0_sqerr[p] = sqrt(min(res$mean_impact_speed0_sqerr[index[p,]]))
-      smallest$absolute_impact_speed_reduction_sqerr[p] = sqrt(min(res$absolute_impact_speed_reduction_sqerr[index[p,]]))
-      smallest$proportion_crashes_avoided_sqerr[p] = sqrt(min(res$proportion_crashes_avoided_sqerr[index[p,]]))
-      smallest$absolute_injury_risk_reduction_sqerr[p] = sqrt(min(res$absolute_injury_risk_reduction_sqerr[index[p,]]))
-      
-    }
-    return(list(aver = aver, 
-                biggest = biggest, 
-                smallest = smallest,
-                res = res))
-  } else{
-    return(res)
+  
+  aver = as.data.frame(matrix(nrow=total_iter,ncol=0))
+  biggest = as.data.frame(matrix(nrow=total_iter,ncol=0))
+  smallest = as.data.frame(matrix(nrow=total_iter,ncol=0))
+  index = matrix(nrow = total_iter,ncol = Sim_n)
+  for(p in 1:total_iter){
+    aver$group[p] = group
+    aver$reduce_simulations_by_logic[p] = reduce_simulations_by_logic
+    index[p,] = seq(p,total_iter*Sim_n,total_iter)
+    aver$neff0[p] = mean(res$neff0[index[p,]])
+    aver$iter[p] = mean(res$iter[p])
+    aver$mean_impact_speed0_sqerr[p] = sqrt(mean(res$mean_impact_speed0_sqerr[index[p,]]))
+    aver$absolute_impact_speed_reduction_sqerr[p] = sqrt(mean(res$absolute_impact_speed_reduction_sqerr[index[p,]]))
+    aver$proportion_crashes_avoided_sqerr[p] = sqrt(mean(res$proportion_crashes_avoided_sqerr[index[p,]]))
+    aver$absolute_injury_risk_reduction_sqerr[p] = sqrt(mean(res$absolute_injury_risk_reduction_sqerr[index[p,]]))
+    
+    aver$sd_mean_impact_speed0_sqerr[p] = sqrt(var(res$mean_impact_speed0_sqerr[index[p,]]))
+    aver$sd_absolute_impact_speed_reduction_sqerr[p] = sqrt(var(res$absolute_impact_speed_reduction_sqerr[index[p,]]))
+    aver$sd_proportion_crashes_avoided_sqerr[p] = sqrt(var(res$proportion_crashes_avoided_sqerr[index[p,]]))
+    aver$sd_absolute_injury_risk_reduction_sqerr[p] = sqrt(var(res$absolute_injury_risk_reduction_sqerr[index[p,]]))
+    
+    biggest$group[p] = group
+    biggest$reduce_simulations_by_logic[p] = reduce_simulations_by_logic
+    biggest$iter[p] = mean(res$iter[p])
+    biggest$neff0[p] = max(res$neff0[index[p,]])
+    biggest$mean_impact_speed0_sqerr[p] = sqrt(max(res$mean_impact_speed0_sqerr[index[p,]]))
+    biggest$absolute_impact_speed_reduction_sqerr[p] = sqrt(max(res$absolute_impact_speed_reduction_sqerr[index[p,]]))
+    biggest$proportion_crashes_avoided_sqerr[p] = sqrt(max(res$proportion_crashes_avoided_sqerr[index[p,]]))
+    biggest$absolute_injury_risk_reduction_sqerr[p] = sqrt(max(res$absolute_injury_risk_reduction_sqerr[index[p,]]))
+    
+    
+    smallest$group[p] = group
+    smallest$reduce_simulations_by_logic[p] = reduce_simulations_by_logic
+    smallest$iter[p] = mean(res$iter[p])
+    smallest$neff0[p] = min(res$neff0[index[p,]])
+    smallest$mean_impact_speed0_sqerr[p] = sqrt(min(res$mean_impact_speed0_sqerr[index[p,]]))
+    smallest$absolute_impact_speed_reduction_sqerr[p] = sqrt(min(res$absolute_impact_speed_reduction_sqerr[index[p,]]))
+    smallest$proportion_crashes_avoided_sqerr[p] = sqrt(min(res$proportion_crashes_avoided_sqerr[index[p,]]))
+    smallest$absolute_injury_risk_reduction_sqerr[p] = sqrt(min(res$absolute_injury_risk_reduction_sqerr[index[p,]]))
+    
   }
+  return(list(aver = aver, 
+              biggest = biggest, 
+              smallest = smallest,
+              res = res))
+  
 }
